7fdb3c288c3cca266f17cc32819aefcb08e90a8d
Yossy Papd <122671554+Gifsho@users.noreply.github.com>
2025-01-15 22:56:28 +0700

    Update login.js

--- public/login.js
+++ public/login.js
@@ -25,7 +25,7 @@ async function login(event) {
   const encryptedPassword = encryptText(Password, encryptionKey, iv);
 
   try {
-    const response = await fetch("http://localhost:3000/login", {
+    const response = await fetch("https://logintest-gxrh.onrender.com/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
